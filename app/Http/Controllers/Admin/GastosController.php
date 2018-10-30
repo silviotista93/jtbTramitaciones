5cface7a46253247fbb6eae68e08f1a9468736a1
silviotista93 <silviotista93@gmail.com>
2018-10-30 17:05:36 -0500

    gasto

--- app/Http/Controllers/Admin/GastosController.php
+++ app/Http/Controllers/Admin/GastosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Gasto;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,5 +11,22 @@ class GastosController extends Controller
     public function index(){
 
         return view('admin.gastos.gastos');
+    }
+
+    /**
+     *
+     */
+    public function store(Request $request){
+        $this->validate($request,[
+           'detalle' => 'required',
+           'valor_gasto' => 'required'
+        ]);
+
+        Gasto::create([
+           'detalle' => strtoupper($request->get('detalle')),
+           'valor' => $request->get('valor')
+        ]);
+
+        return back()->with('flash','Gasto ingresado correctamente');
     }
 }
